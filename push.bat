cb711c6abb004284e9f0ce5e005fcc835870b6e3
SatvikSharma23 <ankurrawat2005@gmail.com>
2025-06-23 01:11:25 +0530

    Initial commit

---
+++ push.bat
@@ -0,0 +1,5 @@
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/ankurrawatll/Space-station-safety-inventory-detection-during-hazard-.-
+git push -u origin main
